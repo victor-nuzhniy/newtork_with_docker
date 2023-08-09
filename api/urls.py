0917a8c4f2a249a347a648ea406695bb5c022290
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-09 09:13:20 +0300

    dev added token auth views to config.urls, created api.urls

---
+++ api/urls.py
@@ -0,0 +1,1 @@
+"""Module for 'api' app urls."""
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("token/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
 ]
