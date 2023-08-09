b4a6e7e2ad63396f7cd346006cabd921bb7bc704
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-09 16:51:55 +0300

    dev added Post and Like models

--- api/models.py
+++ api/models.py
@@ -9,3 +9,29 @@ class User(AbstractUser):
     last_request_at = models.DateTimeField(
         blank=True, null=True, verbose_name="Last request at"
     )
+
+
+class Post(models.Model):
+    """Model for 'api' posts."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Post author")
+    message = models.CharField(max_length=255, verbose_name="Post message")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+
+    def __str__(self) -> str:
+        """Represent model instance."""
+        return f"Posted by {self.user} {self.created_at}"
+
+
+class Like(models.Model):
+    """Model for post like."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Like author")
+    message = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="Post")
+    eval = models.BooleanField(default=False, verbose_name="Evaluation")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+
+    def __str__(self) -> str:
+        """Represent model instance."""
+        return f"Like by {self.user} {self.created_at}"
