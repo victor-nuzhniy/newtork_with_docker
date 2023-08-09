a0f511d401a4b4a2684a4c85a067df8661776aa5
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-09 09:10:42 +0300

    dev corrected model field, added custom admin settings to User model

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,36 @@
 """Module for 'api' app admin site."""
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    """Customized UserAdmin class with additional fields."""
+
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("username", "email")
+    fieldsets = (
+        (None, {"fields": ("username", "last_name", "first_name", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            _("Important dates"),
+            {"fields": ("last_login", "date_joined", "last_request_at")},
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- api/models.py
+++ api/models.py
@@ -6,4 +6,6 @@ from django.db import models
 class User(AbstractUser):
     """Custom User model."""
 
-    last_request_at = models.DateTimeField(verbose_name="Last request at")
+    last_request_at = models.DateTimeField(
+        blank=True, null=True, verbose_name="Last request at"
+    )
