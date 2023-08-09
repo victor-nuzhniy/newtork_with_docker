96c37386ddf0034d967b5698a4fda565c840fffd
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-09 10:08:35 +0300

    dev created api urls and transfer there 'token' and 'token/refresh' endpoints, added 'register' endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,11 @@
 """Module for 'api' app urls."""
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .views import RegisterView
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    path("register/", RegisterView.as_view(), name="sign_up"),
+]
