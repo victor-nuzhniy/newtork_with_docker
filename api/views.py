d884ee1526206c56015e49b456bced51b148d73e
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-09 10:15:03 +0300

    dev added RegisterView

--- api/views.py
+++ api/views.py
@@ -1,1 +1,17 @@
 """Class and function views for 'api' app."""
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+
+class RegisterView(APIView):
+    """Class view for user registering."""
+
+    def post(self, request: Request) -> Response:
+        """Post data to create User."""
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
