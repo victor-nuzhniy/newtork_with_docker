952f25d66c388c78d9861a6f3faceb2dab73ece6
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-09 17:45:19 +0300

    dev added PostCreateView and its url

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegisterView
+from .views import PostCreateView, RegisterView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
     path("signup/", RegisterView.as_view(), name="sign_up"),
+    path("post/", PostCreateView.as_view(), name="create_post"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 """Class and function views for 'api' app."""
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from .models import Post
 from .schemas import user_register_schema
-from .serializers import UserSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
 class RegisterView(APIView):
@@ -18,3 +22,16 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class PostCreateView(generics.CreateAPIView):
+    """Class with only POST method for creating message (post)."""
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def perform_create(self, serializer):
+        """Add user to serializer and save Post instance."""
+        serializer.save(user=self.request.user)
