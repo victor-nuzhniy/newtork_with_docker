45ea7fed99e08faaa84225f583fff5be64318b70
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-10 22:59:45 +0300

    dev refactor view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,7 @@
 """Class and function views for 'api' app."""
-from datetime import datetime
+from typing import Dict
 
-from django.db.models import Count, F
-from pytz import utc
+from django.db.models import QuerySet
 from rest_framework import generics, status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.request import Request
@@ -10,11 +9,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from .models import Like, Post, User
+from .models import User
 from .schemas import LikeSchema, analitics_schema, user_register_schema
 from .serializers import LikeSerializer, PostSerializer, UserSerializer
 from .services.mixins import UpdateRequestFieldMixin
-from .services.model_operations import get_like_instance
+from .services.model_operations import (
+    get_analitic_like_queryset,
+    get_like_instance,
+    get_post_queryset,
+    get_user_instance_data,
+)
+from .services.utils import get_like, process_date_input
 
 
 class RegisterView(APIView):
@@ -22,19 +27,24 @@ class RegisterView(APIView):
 
     schema = user_register_schema
 
-    @staticmethod
-    def post(request: Request) -> Response:
+    def post(self, request: Request) -> Response:
         """Post data to create User."""
-        serializer = UserSerializer(data=request.data)
+        user_data = self.perform_create(request.data)
+        return Response(user_data)
+
+    @staticmethod
+    def perform_create(data: Dict) -> Dict:
+        """Process input data and save user instance."""
+        serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return serializer.data
 
 
 class PostCreateView(UpdateRequestFieldMixin, generics.CreateAPIView):
     """Class with only POST method for creating message (post)."""
 
-    queryset = Post.objects.all()
+    queryset = get_post_queryset()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
@@ -51,8 +61,7 @@ class LikeView(UpdateRequestFieldMixin, APIView):
     authentication_classes = [JWTAuthentication]
     schema = LikeSchema()
 
-    @staticmethod
-    def post(request, format=None) -> Response:
+    def post(self, request) -> Response:
         """
         Create Like instance.
 
@@ -63,14 +72,16 @@ class LikeView(UpdateRequestFieldMixin, APIView):
         message_id: int = request.data.get("message_id")
         user: User = request.user
         eval_data = request.data.get("eval").lower()
-        if eval_data == "like":
-            like: bool = True
-        elif eval_data == "dislike":
-            like = False
-        else:
+        if like := get_like(eval_data):
             return Response(
                 {"result": "Invalid input data."}, status=status.HTTP_406_NOT_ACCEPTABLE
             )
+        like_data = self.perform_create(message_id, user, like)
+        return Response(like_data)
+
+    @staticmethod
+    def perform_create(message_id: int, user: User, like: bool) -> Dict:
+        """Process input data and save Like instance."""
         serializer = LikeSerializer(
             data={
                 "user": user.pk,
@@ -80,17 +91,21 @@ class LikeView(UpdateRequestFieldMixin, APIView):
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return serializer.data
 
-    @staticmethod
-    def delete(request, format=None) -> Response:
+    def delete(self, request) -> Response:
         """Delete Like instance."""
         if like := get_like_instance(request.user, request.data.get("message_id")):
-            like.delete()
+            self.perform_delete(like)
             return Response({"result": "Like was successfully deleted."})
         return Response(
             {"result": "Like was not found"}, status=status.HTTP_404_NOT_FOUND
         )
+
+    @staticmethod
+    def perform_delete(like: QuerySet) -> None:
+        """Delete fetched instances."""
+        like.delete()
 
 
 class AnaliticView(UpdateRequestFieldMixin, APIView):
@@ -101,35 +116,19 @@ class AnaliticView(UpdateRequestFieldMixin, APIView):
     schema = analitics_schema
 
     @staticmethod
-    def get(request: Request, format=None) -> Response:
+    def get(request: Request) -> Response:
         """
         Handle input and return analitics data.
 
         Input format - '%Y-%m-%d'.
         Return list with dates and likes per date in given range.
         """
-        try:
-            date_from = datetime.now(utc).strptime(
-                request.query_params.get("date_from"), "%Y-%m-%d"
-            )
-            date_to = datetime.now(utc).strptime(
-                request.query_params.get("date_to"), "%Y-%m-%d"
-            )
-        except ValueError:
+        if input_data := process_date_input(request.query_params) is None:
             return Response(
                 {"result": "Invalid input format."},
                 status=status.HTTP_406_NOT_ACCEPTABLE,
             )
-
-        data = (
-            Like.objects.filter(created_at__range=(date_from, date_to))
-            .annotate(date=F("created_at__date"))
-            .values("date")
-            .order_by("date")
-            .annotate(likes=Count("eval"))
-        )
-
-        return Response({"analitics": list(data)})
+        return Response({"analitics": get_analitic_like_queryset(input_data)})
 
 
 class UserActivityView(APIView):
@@ -139,13 +138,13 @@ class UserActivityView(APIView):
     authentication_classes = [JWTAuthentication]
 
     @staticmethod
-    def get(request, pk, format=None) -> Response:
+    def get(request, pk) -> Response:
         """
         Get user last_login and last_request_at data.
 
         Path parameter 'pk' - user pk to look activity at.
         """
-        user = User.objects.filter(id=pk).only("last_login", "last_request_at").first()
+        user = get_user_instance_data(pk)
         if user:
             return Response(
                 {
