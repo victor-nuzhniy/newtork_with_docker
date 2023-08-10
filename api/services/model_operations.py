9bb672023fdb332e69bfe50bba9f740d7f8012d4
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-10 22:59:19 +0300

    dev refactor: added functions to model_operations

--- api/services/model_operations.py
+++ api/services/model_operations.py
@@ -1,10 +1,33 @@
 """Module for 'api' app model operations."""
+from typing import List, Optional, Tuple
 
-from django.db.models import QuerySet
+from django.db.models import Count, F, QuerySet
 
-from api.models import Like, User
+from api.models import Like, Post, User
 
 
 def get_like_instance(user: User, message_id: int) -> QuerySet:
     """Get Like instance by user and message_id."""
     return Like.objects.filter(user=user, message=message_id)
+
+
+def get_analitic_like_queryset(input_data: Tuple) -> List:
+    """Get Like queryset for analitic view and return as a list."""
+    date_from, date_to = input_data
+    return list(
+        Like.objects.filter(created_at__range=(date_from, date_to))
+        .annotate(date=F("created_at__date"))
+        .values("date")
+        .order_by("date")
+        .annotate(likes=Count("eval"))
+    )
+
+
+def get_user_instance_data(pk: int) -> Optional[User]:
+    """Get User instance last_login and last_request_at by pk."""
+    return User.objects.filter(id=pk).only("last_login", "last_request_at").first()
+
+
+def get_post_queryset() -> QuerySet:
+    """Get Post model queryset."""
+    return Post.objects.all()
