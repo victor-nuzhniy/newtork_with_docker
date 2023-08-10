c7c2ccebebf9e2ee6cc81eb29628eb06009ba45b
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-10 13:57:36 +0300

    dev added LikeSchema to schemas

--- api/schemas.py
+++ api/schemas.py
@@ -1,4 +1,6 @@
 """Schemas module for 'api' app."""
+from typing import List
+
 import coreapi
 from rest_framework.schemas import AutoSchema
 
@@ -21,3 +23,30 @@ user_register_schema = AutoSchema(
         ),
     ],
 )
+
+
+class LikeSchema(AutoSchema):
+    """Class for Like operation schema."""
+
+    manual_fields: List = []
+
+    def get_manual_fields(self, path, method) -> List:
+        """Get manual_fields for different for POST and DELETE methods."""
+        custom_fields: List = [
+            coreapi.Field(
+                name="message_id",
+                required=True,
+                location="form",
+                description="Message id",
+            ),
+        ]
+        if method.lower() == "post":
+            custom_fields += [
+                coreapi.Field(
+                    name="eval",
+                    required=True,
+                    location="form",
+                    description="Like value",
+                ),
+            ]
+        return self._manual_fields + custom_fields
