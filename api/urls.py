ba7e10873d25d2320f5a85cb440ab18b7888f1c3
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-10 13:58:40 +0300

    dev added "like" urls

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import PostCreateView, RegisterView
+from .views import LikeView, PostCreateView, RegisterView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
     path("signup/", RegisterView.as_view(), name="sign_up"),
     path("post/", PostCreateView.as_view(), name="create_post"),
+    path("like/", LikeView.as_view(), name="like"),
 ]
