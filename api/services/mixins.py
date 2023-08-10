d1d6b6bdc5329396dd29ac042905324c7baa32a7
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-10 18:58:16 +0300

    dev created mixin.UpdateRequestFieldMixin

---
+++ api/services/mixins.py
@@ -0,0 +1,13 @@
+"""Mixins module for 'api' app."""
+from rest_framework.request import Request
+
+
+class UpdateRequestFieldMixin:
+    """Class for adding functionality for updating user last_request_at field."""
+
+    @staticmethod
+    def perform_authentication(request: Request) -> None:
+        """Add additional save operation for updating User last_request_at field."""
+        user = request.user
+        if user.is_authenticated:
+            user.save()
