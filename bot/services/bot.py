c170dfb4da05cf5e1feb459b0640b75b796cbccc
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-11 21:29:21 +0300

    dev-bot refactor bot

--- bot/services/bot.py
+++ bot/services/bot.py
@@ -29,17 +29,19 @@ class Bot:
     def create_users_and_tasks(self) -> None:
         """Create given in config data number of users."""
         number_of_users = self.config.get_var("number_of_users")
-        max_posts_per_user = self.config.get_var("max_posts_per_user")
-        max_likes_per_user = self.config.get_var("max_likes_per_user")
         for index in range(number_of_users):
             username: str = fake.unique.first_name() + "_" + fake.last_name()
             password: str = fake.pystr(min_chars=50, max_chars=60)
-            posts_number: int = fake.random_int(min=1, max=max_posts_per_user)
-            likes_number: int = fake.random_int(min=1, max=max_likes_per_user)
+            self.add_task(index, "max_posts_per_user", self.post_tasks)
+            self.add_task(index, "max_likes_per_user", self.like_tasks)
             self.users_data.append({"username": username, "password": password})
-            self.post_tasks[index] = posts_number
-            self.like_tasks[index] = likes_number
             register_user(username, password)
+
+    def add_task(self, index: int, var_name: str, container: Dict) -> None:
+        """Add task to its container."""
+        max_number: int = self.config.get_var(var_name)
+        number: int = fake.random_int(min=1, max=max_number)
+        container[index] = number
 
     def get_users_tokens(self) -> None:
         """Fetch user access token from 'network' api."""
