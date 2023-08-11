ef76d1681181b8c28a72b88c23fada60a37ae564
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-11 18:23:15 +0300

    dev-bot refactor bot create_users_and_tasks method

--- bot/services/bot.py
+++ bot/services/bot.py
@@ -22,6 +22,8 @@ class Bot:
         self.post_tasks: Dict = dict()
         self.like_tasks: Dict = dict()
         self.users_data: List = []
+        self.posts_number: int = 0
+        self.user_tokens: List = []
 
     def create_users_and_tasks(self) -> None:
         """Create given in config data number of users."""
@@ -31,7 +33,10 @@ class Bot:
         for index in range(number_of_users):
             username: str = fake.unique.first_name() + "_" + fake.last_name()
             password: str = fake.pystr(min_chars=50, max_chars=60)
+            posts_number: int = fake.random_int(min=1, max=max_posts_per_user)
+            likes_number: int = fake.random_int(min=1, max=max_likes_per_user)
             self.users_data.append({"username": username, "password": password})
-            self.post_tasks[index] = fake.random_int(max=max_posts_per_user - 1)
-            self.like_tasks[index] = fake.random_int(max=max_likes_per_user - 1)
+            self.post_tasks[index] = posts_number
+            self.like_tasks[index] = likes_number
+            self.posts_number += posts_number
             register_user(username, password)
