0012b8de5613d1a2bc9be243b30368a379119baa
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-11 18:22:01 +0300

    dev-bot added service/handlers create_post

--- bot/services/handlers.py
+++ bot/services/handlers.py
@@ -12,3 +12,12 @@ def register_user(username: str, password: str) -> None:
     headers: Dict = dict()
     data: Dict = {"username": username, "password": password}
     make_request(url, method, headers, data)
+
+
+def create_post(token: str, message: str) -> None:
+    """Create post by user."""
+    url: str = urls.get_post_create_url()
+    method: str = "post"
+    headers: Dict = {"Authorization": f"Bearer {token}"}
+    data: Dict = {"message": message}
+    make_request(url, method, headers, data)
