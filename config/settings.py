68d8b50ba900fcd4ad9c4e07483ea0b8fb0fabd7
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-11 11:25:58 +0300

    added date_input_format in drf settings

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
-    'DATE_FORMAT': "%Y-%m-%d",
+    "DATE_FORMAT": "%Y-%m-%d",
+    "DATE_INPUT_FORMAT": ["%Y-%m-%d"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
