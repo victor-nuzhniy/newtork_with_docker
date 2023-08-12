a4964896e21f65dc5bf17b23634f3eb809659289
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-12 12:24:41 +0300

    dev-test created api factories.py

---
+++ tests/api/factories.py
@@ -0,0 +1,63 @@
+"""Factories for testing 'api' app models."""
+import factory
+
+from api.models import Like, Post, User
+from tests.bases import BaseModelFactory
+
+
+class UserFactory(BaseModelFactory):
+    """Factory for testing custom User model."""
+
+    class Meta:
+        """Class Meta for UserFactory."""
+
+        model = User
+        exclude = ("post_set", "like_set")
+
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    is_staff = factory.Faker("pybool")
+    is_active = factory.Faker("pybool")
+    post_set = factory.RelatedFactoryList(
+        factory="tests.api.factories.PostFactory",
+        factory_related_name="post_set",
+        size=0,
+    )
+    like_set = factory.RelatedFactoryList(
+        factory="tests.api.factories.LikeFactory",
+        factory_related_name="like_set",
+        size=0,
+    )
+
+
+class PostFactory(BaseModelFactory):
+    """Factory for testing Post model."""
+
+    class Meta:
+        """Class Meta for PostFactory."""
+
+        model = Post
+        django_get_or_create = ("user",)
+        exclude = ("like_set",)
+
+    user = factory.SubFactory(UserFactory)
+    message = factory.Faker("pystr", min_chars=1, max_chars=255)
+    like_set = factory.RelatedFactoryList(
+        factory="tests.api.factories.LikeFactory",
+        factory_related_name="like_set",
+        size=0,
+    )
+
+
+class LikeFactory(BaseModelFactory):
+    """Factory for testing Like model."""
+
+    class Meta:
+        """Class Meta for LikeFactory."""
+
+        model = Like
+        django_get_or_create = ("user", "message")
+
+    user = factory.SubFactory(UserFactory)
+    message = factory.SubFactory(PostFactory)
+    eval = factory.Faker("pybool")
