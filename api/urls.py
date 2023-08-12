a62d8986191fcef890751593d613c9f9adccf1b2
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-12 11:05:09 +0300

    dev-bot changed api urls

--- api/urls.py
+++ api/urls.py
@@ -14,14 +14,14 @@ from .views import (
 )
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("signup/", RegisterView.as_view(), name="sign_up"),
+    path("auth/token/", TokenObtainPairView.as_view(), name="token"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/signup/", RegisterView.as_view(), name="sign_up"),
     path("post/", PostCreateView.as_view(), name="create_post"),
-    path("like/", LikeView.as_view(), name="like"),
-    path("analitics/", AnaliticView.as_view(), name="analitics"),
-    path("activity/<int:pk>/", UserActivityView.as_view(), name="activity"),
-    path("statistic/", StatisticView.as_view(), name="statistic"),
     path("post/last/", LastPostsView.as_view(), name="last_posts"),
+    path("like/", LikeView.as_view(), name="like"),
     path("like/last/", LastLikesView.as_view(), name="last_likes"),
+    path("analitics/", AnaliticView.as_view(), name="analitics"),
+    path("analitics/activity/<int:pk>/", UserActivityView.as_view(), name="activity"),
+    path("analitics/statistic/", StatisticView.as_view(), name="statistic"),
 ]
