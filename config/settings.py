4f9265ab744e06658322f775ac65103897f3acd1
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-26 19:22:02 +0300

    dev hide ALLOWED_HOSTS in .env

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
