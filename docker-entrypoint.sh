55a0b9b245f00728af972fc62b6df1a07dff1457
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-27 14:45:36 +0300

    dev fixed docker-entrypoint.sh issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,21 +2,21 @@
 
 echo "Make database migrations at startup of project."
 
-while ! python manage.py makemigrations 2>$1; do
+while ! python manage.py makemigrations api 2>&1; do
   echo "Making migrations is in progress status"
   sleep 3
 done
 
 echo "Migrate the database at startup of project."
 
-while ! python manage.py migrate 2>$1; do
+while ! python manage.py migrate 2>&1; do
   echo "Migration is in progress status."
   sleep 3
 done
 
 echo "Create superuser if it had not been created earlier."
 
-while ! python manage.py init_create_superuser 2>$1; do
+while ! python manage.py init_create_superuser 2>&1; do
   echo "Create initial superuser, if it had not been created earlier."
   sleep 3
 done
